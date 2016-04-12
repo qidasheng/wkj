79e257ad95497a25d72479fa998943621efb5844
qidasheng <qsf.zzia1@hotmail.com>
2016-04-12 17:32:11 +0800

    update

---
+++ src/app/controller/base.php
@@ -0,0 +1,7 @@
+<?php
+class controller_base {
+        public function run() {
+                echo "@".date('Y-m-d H:i:s')."
+";
+        }
+}
---
+++ src/app/controller/test/a.php
@@ -0,0 +1,8 @@
+<?php
+class controller_test_a extends controller_base {
+
+	public function index() {
+		$this->run();
+	}
+}
+
